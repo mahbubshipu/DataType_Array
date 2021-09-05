a9130ef912e5f470a1bf92a41da1c38d04edb3a5
Md. Mahbubul Alom Shipu <shipumdmahbubulalom@gmail.com>
2021-09-05 18:11:08 +0600

    two more projects added

---
+++ DataType_Solution/InputOutput/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace InputOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* string line = Console.ReadLine();
+
+             while (true)
+             {
+                 int charValue = Console.Read();
+                 char c = (char)charValue;
+                 Console.WriteLine(c);
+             }
+             ConsoleKeyInfo key = Console.ReadKey();
+
+             Console.WriteLine("Hello World!");
+            */
+            //var validNumber = Console.ReadLine();
+            //int validNum;
+            //bool successfulConverted = int.TryParse(validNumber, out validNum);
+            //Console.WriteLine(successfulConverted);
+
+            //var line = Console.ReadLine();
+            //var lineparts=line.Split(',');
+
+            //var wholeNumber = int.Parse(lineparts[0]);
+            //var fractionNumber = double.Parse(lineparts[1]);
+
+            //Turnering operator
+            int x = 5;
+
+            var message = x > 4 ? "This is correct" : "This is not correct";
+            Console.WriteLine(message);
+
+        }
+    }
+}
---
+++ DataType_Solution/Operators/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = x++;
+            int z = ++x;
+            Console.WriteLine(z);
+            if (x > 4)
+            {
+                Console.WriteLine("My name is Shipu");
+            }
+            else if(x>5){
+                Console.WriteLine("My name is Jerin");
+            }
+            else if(x>6){
+                Console.WriteLine("My name is Sakib");
+            }
+            else
+            {
+                Console.WriteLine("My name is Kichu e na");
+            }
+        }
+    }
+}
